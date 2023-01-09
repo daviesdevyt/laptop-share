6d10ac1de595174c70300a7e578054017b469aad
unknown <davidonzan@gmail.com>
2023-01-09 17:26:00 +0100

    Refactor and authorization code connection

--- backend/server_advertiser.py
+++ backend/server_advertiser.py
@@ -1,28 +1,37 @@
-import os, socket, time, json, threading
+import os, socket, time, json, threading, random
 
 class ServerAdvertiser():
 
     socket_close = False
-    server_details = {
-        "username": os.getlogin(),
-        "time": time.time()
-    }
+    server_details = {"username": os.getlogin()}
 
-    dest_addr = ('255.255.255.255', 9999)
+    PORT = 9999
+    auth_code = str(random.randint(1000, 9999))
+    dest_addr = ('255.255.255.255', PORT)
+    connected_clients: set
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     
     def __init__(self) -> None:
-        self.start_server()
+        threading.Thread(target=self.broadcast_server).start()
+        threading.Thread(target=self.authorize).start()
 
-    def start_server(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        threading.Thread(target=self.broadcast_server, args=(sock,)).start()
-
-    def broadcast_server(self, sock):
+    def broadcast_server(self):
         while not self.socket_close:
-            sock.sendto(json.dumps(self.server_details).encode(), self.dest_addr)
+            self.server_details["time"] = time.time()
+            self.sock.sendto(json.dumps(self.server_details).encode(), self.dest_addr)
             time.sleep(1)
-        sock.close()
     
+    def authorize(self):
+        while not self.socket_close:
+            code, addr = self.sock.recvfrom(1024)
+            if code.decode() == self.auth_code:
+                self.sock.sendto(self.auth_code.encode(), (addr, self.PORT))
+                self.connected_clients.add(addr)
+            else:
+                self.sock.sendto(b"None", (addr, self.PORT))
+
     def close(self):
         self.socket_close = True
+        self.sock.close()
--- backend/server_browser.py
+++ backend/server_browser.py
@@ -1,15 +1,19 @@
 import socket, time, json
-import threading
+import threading, copy
 
 
 class ServerBrowser():
 
     known_servers = {}
     socket_close = False
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('0.0.0.0', 9999))
 
     def __init__(self) -> None:
         threading.Thread(target=self.remove_dull_servers).start()
-        self.detect_servers()
+        threading.Thread(target=self.recieve_server_info).start()
 
     def remove_dull_servers(self):
         while not self.socket_close:
@@ -17,23 +21,32 @@ class ServerBrowser():
                 if time.time()-self.known_servers[server]["time"] > 3:
                     self.known_servers.pop(server)
             time.sleep(1)
-
-    def detect_servers(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind(('0.0.0.0', 9999))
-        threading.Thread(target=self.recieve_server_info, args=(sock,)).start()
     
-    def recieve_server_info(self, sock):
+    def recieve_server_info(self):
         while not self.socket_close:
-            server_details, addr = sock.recvfrom(1024)
+            server_details, addr = self.sock.recvfrom(1024)
             server_details = json.loads(server_details.decode())
             self.known_servers[addr] = server_details
-        sock.close()
+            time.sleep(0.1)
     
+    def connect(self, ip: str, code:str):
+        sock = copy.deepcopy(self.sock)
+        sock.settimeout(5)
+        sock.sendto(code.encode(), (ip, 9999))
+        details, addr = sock.recvfrom(1024)
+        try:
+            if details == code.encode() and addr == ip:
+                sock.close()
+                self.close()
+                return addr
+            elif details == b"None":
+                return "Incorrect pin"
+        except socket.timeout:
+            return "Could not connect to network"
+
     def get_servers(self):
         return self.known_servers
 
     def close(self):
         self.socket_close = True
+        self.sock.close()
