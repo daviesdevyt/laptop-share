06b82cd75a5fd01e021db854faeb22bf8b31067a
unknown <davidonzan@gmail.com>
2023-01-10 18:06:03 +0100

    Client  reciever

---
+++ backend/client.py
@@ -0,0 +1,32 @@
+import socket, threading, json, math
+
+class ServerReciever():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    PORT = 10000
+    progress = 0
+
+    def __init__(self, ip) -> None:
+        self.sock.connect((ip, self.PORT))
+        threading.Thread(target=self.recieve_file).start()
+    
+    def recieve_file(self):
+        threading.Thread(target=self.build_file).start()
+
+    def build_file(self):
+        file_details = json.loads(self.sock.recv(1024).decode())
+        file = open(file_details["name"], "wb")
+        filebytes = b""
+        while True:
+            buffer_size = 1024*math.floor(file_details["size"]/1000)
+            buffer_size = min(buffer_size, 1024*10000)
+            data = self.sock.recv(buffer_size)
+            if filebytes[-5:] == b"<END>":
+                break
+            filebytes += data
+            self.progress += buffer_size
+        file.write(filebytes)
+        file.close()
+    
+    def get_progress(self):
+        return self.progress
+
