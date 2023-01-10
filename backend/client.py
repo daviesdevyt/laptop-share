33ba67cb843184d0389a62c9bad20712671a19c5
unknown <davidonzan@gmail.com>
2023-01-10 19:03:28 +0100

    Client sending edit

--- backend/client.py
+++ backend/client.py
@@ -7,25 +7,24 @@ class ServerReciever():
 
     def __init__(self, ip) -> None:
         self.sock.connect((ip, self.PORT))
-        threading.Thread(target=self.recieve_file).start()
-    
-    def recieve_file(self):
         threading.Thread(target=self.build_file).start()
 
     def build_file(self):
-        file_details = json.loads(self.sock.recv(1024).decode())
-        file = open(file_details["name"], "wb")
-        filebytes = b""
         while True:
-            buffer_size = 1024*math.floor(file_details["size"]/1000)
-            buffer_size = min(buffer_size, 1024*10000)
-            data = self.sock.recv(buffer_size)
-            if filebytes[-5:] == b"<END>":
-                break
-            filebytes += data
-            self.progress += buffer_size
-        file.write(filebytes)
-        file.close()
+            file_details = json.loads(self.sock.recv(1024).decode())
+            file = open(file_details["name"], "wb")
+            filebytes = b""
+            while True:
+                data = self.sock.recv(buffer_size)
+                buffer_size = 1024*math.floor(file_details["size"]/1000)
+                buffer_size = min(buffer_size, 1024*10000)
+                data = self.sock.recv(buffer_size)
+                if filebytes[-5:] == b"<END>":
+                    break
+                filebytes += data
+                self.progress += buffer_size
+            file.write(filebytes)
+            file.close()
     
     def get_progress(self):
         return self.progress
