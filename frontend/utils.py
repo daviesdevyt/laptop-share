9863a91ec019ab9d66ca1756e261a49746b83bb4
unknown <davidonzan@gmail.com>
2023-01-24 16:02:03 +0100

    Recieve page and Window size Adjustment

--- frontend/utils.py
+++ frontend/utils.py
@@ -1,6 +1,6 @@
 import customtkinter
 
-window_size = "450x520"
+window_size = "360x520"
 title = "Laptop Share"
 
 customtkinter.set_appearance_mode("System")
@@ -9,3 +9,4 @@ customtkinter.set_default_color_theme("blue")
 app = customtkinter.CTk()
 app.geometry(window_size)
 app.title(title)
+app.minsize(360, 520)
