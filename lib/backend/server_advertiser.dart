7f3ba26b165f7603aaf269e1a98f25b49913a46b
xandee <davidonzan@gmail.com>
2023-02-10 13:17:19 +0100

    server polling and server closing

--- lib/backend/server_advertiser.dart
+++ lib/backend/server_advertiser.dart
@@ -1,26 +1,33 @@
 import 'dart:async';
 import 'dart:io';
 import 'dart:convert';
+import 'package:cron/cron.dart';
 
 class ServerAdvertiser {
   late RawDatagramSocket socket;
-  Map serverDetails = {
-    "username": Platform.environment["USERNAME"]
-  };
+  Map serverDetails = {"username": Platform.environment["USERNAME"]};
+  final cron = Cron();
+
   ServerAdvertiser() {
-    runBroadcastClient();
+    cron.schedule(Schedule.parse("*/2 * * * * *"), broadcastServer);
   }
 
-  Future<void> runBroadcastClient() async {
+  Future<void> broadcastServer() async {
     // Create a broadcast client
     socket = await RawDatagramSocket.bind(InternetAddress.anyIPv4, 0);
     socket.broadcastEnabled = true;
+    serverDetails["t"] = DateTime.now().millisecondsSinceEpoch;
     socket.send(
       utf8.encode(json.encode(serverDetails)),
       InternetAddress("255.255.255.255"),
       4000,
     );
     print('Sent broadcast message: $serverDetails');
+  }
+
+  void close() async {
+    cron.close();
     socket.close();
   }
 }
+  
