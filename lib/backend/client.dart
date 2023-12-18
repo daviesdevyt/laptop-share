e95d55dec6caeee33408f73a86dbf9a269166f3f
Duks31 <chidubemjan31@gmail.com>
2023-12-18 06:16:45 +0100

    calulated file size and working of progress bar

--- lib/backend/client.dart
+++ lib/backend/client.dart
@@ -5,6 +5,7 @@ class Client {
   late Socket client;
   late String currentFileName;
   late IOSink sink;
+  late String currentFileSize;
 
   Future<void> connect(ip) async {
     client = await Socket.connect(InternetAddress(ip), 4000);
@@ -30,6 +31,11 @@ class Client {
         print(currentFileName);
         return;
       }
+      if (text.substring(0, 7) == "<fsize>") {
+        currentFileSize = text.substring(7, text.length);
+        print(currentFileSize);
+        return;
+      }
     } catch (e) {}
     File file = await File(
             "C:\\Users\\${Platform.environment["username"]}\\Documents\\LaptopShare\\$currentFileName")
@@ -40,10 +46,30 @@ class Client {
 
   void sendFile(String path) async {
     File file = File(path);
-    List<int> data = await file.readAsBytes();
+    int totalFileSize = await file.length();
+    int bytesSent = 0;
+
+    RandomAccessFile inputFile = await file.open(mode: FileMode.read);
+    final chunkSize = 1024 * 1024;
+
     client.add(utf8.encode("<fname>${file.uri.pathSegments.last}"));
     sleep(Duration(milliseconds: 500));
-    client.add(data);
+
+    while (bytesSent < totalFileSize) {
+      List<int> data = await inputFile.read(chunkSize);
+      client.add(data);
+      bytesSent += data.length;
+      print('Sent ${bytesSent / totalFileSize * 100}%');
+      double progressValue = bytesSent / totalFileSize;
+
+      setState(() {
+        _progressValue = progressValue;
+      });
+
+      sleep(Duration(milliseconds: 500));
+    } 
+    await inputFile.close();
+
     print('File sent successfully');
   }
 
